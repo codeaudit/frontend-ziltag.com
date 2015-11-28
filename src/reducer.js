a0e2391218699a2bcd9d3738cbba4621ed495834
floydwch <floydwch@gmail.com>
2015-11-28 15:54:17 +0800

    eslint indent

--- src/reducer.js
+++ src/reducer.js
@@ -3,23 +3,23 @@ import { routerStateReducer } from 'redux-router'
 
 
 function user_info(state={}, action) {
-	switch (action.type) {
-		case 'USER_INFO_FETCHED':
-			return action.payload.value
-		default:
-			return state
-	}
+  switch (action.type) {
+    case 'USER_INFO_FETCHED':
+      return action.payload.value
+    default:
+      return state
+  }
 }
 
 function avatar_menu(state={}, action) {
-	switch (action.type) {
-		case 'ACTIVATE_AVATAR_MENU':
-			return {avatar_menu_activated: true}
-		case 'DEACTIVATE_AVATAR_MENU':
-			return {avatar_menu_activated: false}
-		default:
-			return state
-	}
+  switch (action.type) {
+    case 'ACTIVATE_AVATAR_MENU':
+      return {avatar_menu_activated: true}
+    case 'DEACTIVATE_AVATAR_MENU':
+      return {avatar_menu_activated: false}
+    default:
+      return state
+  }
 }
 
 export default combineReducers({
