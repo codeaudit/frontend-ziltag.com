b286a85e59c7c9357414c75aedd1e0b3f3fab79f
floydwch <floydwch@gmail.com>
2015-12-01 02:44:08 +0800

    prompt for ziltag map

--- src/component/ZiltagMap/index.js
+++ src/component/ZiltagMap/index.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react'
 
 
+try {
+  if (__WEBPACK__) {
+    require('./index.css')
+  }
+} catch (e) {}
+
+
 class ZiltagMap extends Component {
   render() {
     const {
@@ -10,6 +17,7 @@ class ZiltagMap extends Component {
     return (
       <div className='ziltag-ziltag-map'>
         <img className='ziltag-ziltag-map-src' src={data.src}/>
+        <div className='ziltag-ziltag-map-prompt'>click anywhere to tag</div>
       </div>
     )
   }
