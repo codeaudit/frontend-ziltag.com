eca386f373040546df3d986d2fb81aee4fdedc2a
floydwch <floydwch@gmail.com>
2015-12-01 16:56:55 +0800

    honor BEM

--- src/component/ZiltagMap/index.js
+++ src/component/ZiltagMap/index.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 
+import Ziltag from '../Ziltag'
+
 
 try {
   if (__WEBPACK__) {
@@ -14,10 +16,33 @@ class ZiltagMap extends Component {
       data
     } = this.props
 
+    console.log(data)
+
+    data.width = 340
+    data.height = 340
+
+    const ziltags = data.ziltags && data.ziltags.map(
+      ziltag => {
+        const enhanced_ziltag = Object.assign({}, ziltag)
+        enhanced_ziltag.x = ziltag.x * data.width
+        enhanced_ziltag.y = ziltag.y * data.height
+        return <Ziltag data={enhanced_ziltag} key={ziltag.id}/>
+      }
+    )
+
     return (
       <div className='ziltag-ziltag-map'>
-        <img className='ziltag-ziltag-map-src' src={data.src}/>
-        <div className='ziltag-ziltag-map-prompt'>click anywhere to tag</div>
+        <img className='ziltag-ziltag-map__src' src={data.src}/>
+        <div className='ziltag-ziltag-map__prompt'>click anywhere to tag</div>
+        <div
+          style={{
+            width: data.width,
+            height: data.height
+          }}
+          className='ziltag-ziltag-map__container'
+        >
+          {ziltags}
+        </div>
       </div>
     )
   }
