4eee647e8936be20a21b14dd43577e7152aeec9a
Jian Weihang <tonytonyjan@gmail.com>
2015-12-02 18:19:24 +0800

    mock

--- src/component/ZiltagMapLoginForm/index.js
+++ src/component/ZiltagMapLoginForm/index.js
@@ -15,7 +15,29 @@ class ZiltagMapLoginForm extends Component {
     } = this.props
 
     return (
-      <div className='ziltag-ziltag-map-login-form'>
+      <div className="interactive_dialog">
+        <div className="interactive_dialog__content">
+          <form className="auth-form">
+            <div className="auth-form__fields">
+              <div className="field">
+                <input type="text" className="field__input" placeholder="Email" />
+                <img src="email.png" className="field__icon" />
+              </div>
+              <div className="field">
+                <input type="text" className="field__input" placeholder="Password" />
+                <img src="password.png" className="field__icon" />
+              </div>
+              <div className="auth-form__field" />
+            </div>
+            <div className="auth-form__action">Login</div>
+            <div className="auth-form__footer">forget password?</div>
+          </form>
+        </div>
+        <div className="interactive_dialog__actions">
+          <div className="interactive_dialog__action">Sign up</div>
+          <div className="interactive_dialog__action--active">Login</div>
+          <div className="interactive_dialog__cancel">Cancel</div>
+        </div>
       </div>
     )
   }
--- src/component/ZiltagMapSignUpForm/index.js
+++ src/component/ZiltagMapSignUpForm/index.js
@@ -16,6 +16,30 @@ class ZiltagMapSignUpForm extends Component {
 
     return (
       <div className='ziltag-ziltag-map-sign-up-form'>
+        <div className="interactive_dialog">
+          <div className="interactive_dialog__content">
+            <form className="auth-form">
+              <div className="auth-form__fields">
+                <div className="field">
+                  <input type="text" className="field__input" placeholder="Username" />
+                  <img src="username.png" className="field__icon" />
+                </div>
+                <div className="field">
+                  <input type="text" className="field__input" placeholder="Email" />
+                  <img src="email.png" className="field__icon" />
+                </div>
+                <div className="auth-form__field" />
+              </div>
+              <div className="auth-form__action">Sign Up</div>
+              <div className="auth-form__footer">Signing up means you agree with our Terms and Privacy Policy.</div>
+            </form>
+          </div>
+          <div className="interactive_dialog__actions">
+            <div className="interactive_dialog__action--active">Sign up</div>
+            <div className="interactive_dialog__action">Login</div>
+            <div className="interactive_dialog__cancel">Cancel</div>
+          </div>
+        </div>
       </div>
     )
   }
