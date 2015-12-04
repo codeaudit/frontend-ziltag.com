0aec38327c9d784f9fc4f82c7b6a6452b20c4d89
floydwch <floydwch@gmail.com>
2015-12-05 07:51:09 +0800

    integrate login and sign up on ZiltagMap

--- src/component/ZiltagMapWarn/index.js
+++ src/component/ZiltagMapWarn/index.js
@@ -4,11 +4,39 @@ import React, {Component} from 'react'
 class ZiltagMapWarn extends Component {
   render() {
     const {
-      type
+      type,
+      login,
+      sign_up,
+      resend
     } = this.props
 
+    const auth_warn = (
+      <div>
+        should login or sign up
+        <div onClick={sign_up}>Sign Up</div>
+        <div onClick={login}>Login</div>
+      </div>
+    )
+
+    const verify_warn = (
+      <div>
+        should verify
+        <div onClick={resend}>
+          Resend Email
+        </div>
+      </div>
+    )
+
     return (
-      <div>ZiltagMapWarn\'s placeholder</div>
+      <div className='ziltag-ziltag-map-warn'>
+        {
+          type == 'auth'
+          ? auth_warn
+          : type == 'verify'
+          ? verify_warn
+          : ''
+        }
+      </div>
     )
   }
 }
