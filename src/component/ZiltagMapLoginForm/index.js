61831055de3235f4fe3e32af5f6c911d25ea64bc
Jian Weihang <tonytonyjan@gmail.com>
2015-12-05 15:07:48 +0800

    add css prefix

--- src/component/ZiltagMapLoginForm/index.js
+++ src/component/ZiltagMapLoginForm/index.js
@@ -14,8 +14,8 @@ class ZiltagMapLoginForm extends Component {
       <AuthDialog select="login">
         <TextField onChange={onUserChange} name="login" placeholder="Email or Username" icon="email" />
         <TextField onChange={onPasswordChange} name="password" placeholder="Password" icon="password" />
-        <div onClick={onSubmit} className="auth-dialog__submit">Login</div>
-        <div className="auth-dialog__footer auth-dialog__footer--blue"><a href="#">Forget password?</a></div>
+        <div onClick={onSubmit} className="ziltag-ziltag-map-auth-dialog__submit">Login</div>
+        <div className="ziltag-ziltag-map-auth-dialog__footer ziltag-ziltag-map-auth-dialog__footer--blue"><a href="#">Forget password?</a></div>
       </AuthDialog>
     )
   }
--- src/component/ZiltagMapSignUpForm/index.js
+++ src/component/ZiltagMapSignUpForm/index.js
@@ -14,8 +14,8 @@ class ZiltagMapSignUpForm extends Component {
       <ZiltagMapAuthDialog select="sign-up">
         <ZiltagMapTextField onChange={onNameChange} name="username" placeholder="Username" icon="username" />
         <ZiltagMapTextField onChange={onEmailChange} name="email" placeholder="Email" icon="email" />
-        <div onClick={onSubmit} className="auth-dialog__submit">Sign Up</div>
-        <div className="auth-dialog__footer">Signing up means you agree with our <a href="/term_of_service" target="_blank">Terms</a> and <a href="/privacy_policy" target="_blank">Privacy Policy.</a></div>
+        <div onClick={onSubmit} className="ziltag-ziltag-map-auth-dialog__submit">Sign Up</div>
+        <div className="ziltag-ziltag-map-auth-dialog__footer">Signing up means you agree with our <a href="/term_of_service" target="_blank">Terms</a> and <a href="/privacy_policy" target="_blank">Privacy Policy.</a></div>
       </ZiltagMapAuthDialog>
     )
   }
