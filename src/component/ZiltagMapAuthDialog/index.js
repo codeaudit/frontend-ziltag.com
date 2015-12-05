ae778484f9fb5afc445779fd273230880682bf7b
Jian Weihang <tonytonyjan@gmail.com>
2015-12-05 10:45:11 +0800

    fix quoting

--- src/component/ZiltagMapAuthDialog/index.js
+++ src/component/ZiltagMapAuthDialog/index.js
@@ -15,8 +15,8 @@ class ZiltagMapAuthDialog extends React.Component {
           {this.props.children}
         </div>
         <div className="auth-dialog__actions">
-          <div className={classNames("auth-dialog__action", {"auth-dialog__action--active": this.props.select == "sign-up"})}>Sign Up</div>
-          <div className={classNames("auth-dialog__action", {"auth-dialog__action--active": this.props.select == "login"})}>Login</div>
+          <div className={classNames('auth-dialog__action', {'auth-dialog__action--active': this.props.select == 'sign-up'})}>Sign Up</div>
+          <div className={classNames('auth-dialog__action', {'auth-dialog__action--active': this.props.select == 'login'})}>Login</div>
           <div className="auth-dialog__cancel">Cancel</div>
         </div>
       </div>
