96abcd13e7b6bb9d7da60a4791d8ad073749a45f
floydwch <floydwch@gmail.com>
2015-12-11 01:15:45 +0800

    activate form submit

--- src/component/ZiltagMap/index.js
+++ src/component/ZiltagMap/index.js
@@ -120,60 +120,60 @@ class ZiltagMap extends Component {
                 type='verify'
               />
           : ziltag_input.mode == 'login'
-          ? <ZiltagForm
-              onUserChange={login_form_user_changed}
-              onPasswordChange={login_form_password_changed}
-              onSubmit={() => {
-                const {
-                  user,
-                  password
-                } = login_form
-
-                current_user_login(user, password)
-                .then(() => {
-                  fetch_current_user()
-                  deactivate_ziltag_input()
-                })
-              }}
-            >
-              <input type='email' placeholder='Email'/>
-              <input type='password' placeholder='Password'/>
+          ? <ZiltagForm>
+              <input onChange={login_form_user_changed} type='email' placeholder='Email'/>
+              <input onChange={login_form_password_changed} type='password' placeholder='Password'/>
               <nav>
                 <div onClick={ziltag_input_sign_up} className='ziltag-ziltag-form__link'>Sign Up</div>
                 <div onClick={ziltag_input_login} className='ziltag-ziltag-form__link ziltag-ziltag-form__link--activated'>Login</div>
                 <div onClick={deactivate_ziltag_input} className='ziltag-ziltag-form__link' type='cancel'>Cancel</div>
               </nav>
               <footer>
-                <h1>Login</h1>
+                <h1 onClick={() => {
+                  const {
+                    user,
+                    password
+                  } = login_form
+
+                  current_user_login(user, password)
+                  .then(() => {
+                    fetch_current_user()
+                    deactivate_ziltag_input()
+                  })
+                }}
+                className={'ziltag-ziltag-form__submit'}
+              >
+                Login
+              </h1>
                 <p><a>Forget password?</a></p>
               </footer>
             </ZiltagForm>
           : ziltag_input.mode == 'sign_up'
-          ? <ZiltagForm
-              onNameChange={sign_up_form_name_changed}
-              onEmailChange={sign_up_form_email_changed}
-              onSubmit={() => {
-                const {
-                  name,
-                  email
-                } = sign_up_form
-
-                current_user_sign_up(name, email)
-                .then(() => {
-                  fetch_current_user()
-                  deactivate_ziltag_input()
-                })
-              }}
-            >
-              <input type='name' placeholder='Username'/>
-              <input type='email' placeholder='Email'/>
+          ? <ZiltagForm>
+              <input onChange={sign_up_form_name_changed} type='name' placeholder='Username'/>
+              <input onChange={sign_up_form_email_changed} type='email' placeholder='Email'/>
               <nav>
                 <div onClick={ziltag_input_sign_up} className='ziltag-ziltag-form__link ziltag-ziltag-form__link--activated'>Sign Up</div>
                 <div onClick={ziltag_input_login} className='ziltag-ziltag-form__link'>Login</div>
                 <div onClick={deactivate_ziltag_input} className='ziltag-ziltag-form__link' type='cancel'>Cancel</div>
               </nav>
               <footer>
-                <h1>Sign Up</h1>
+                <h1 onClick={() => {
+                  const {
+                    name,
+                    email
+                  } = sign_up_form
+
+                  current_user_sign_up(name, email)
+                  .then(() => {
+                    fetch_current_user()
+                    deactivate_ziltag_input()
+                  })
+                }}
+                className={'ziltag-ziltag-form__submit'}
+              >
+                Sign Up
+              </h1>
                 <p>Signing up means you agree with our <a>Terms</a> and <a>Privacy Policy</a>.</p>
               </footer>
             </ZiltagForm>
