fb463dcbe37d055d5a7fec4ff74e3679bb59db10
floydwch <floydwch@gmail.com>
2015-12-23 07:48:36 +0800

    fix PseudoComment's style

--- src/component/PseudoComment/index.js
+++ src/component/PseudoComment/index.js
@@ -31,65 +31,69 @@ class PseudoComment extends Component {
 
     if (mode == 'sign_up') {
       var component = (
-        <ZiltagForm>
-          <input onChange={sign_up_form_name_changed} type='name' placeholder='Username'/>
-          <input onChange={sign_up_form_email_changed} type='email' placeholder='Email'/>
-          <nav>
-            <div onClick={pseudo_comment_sign_up} className='ziltag-ziltag-form__link ziltag-ziltag-form__link--activated'>Sign Up</div>
-            <div onClick={pseudo_comment_login} className='ziltag-ziltag-form__link'>Login</div>
-            <div onClick={deactivate_pseudo_comment} className='ziltag-ziltag-form__link' type='cancel'>Cancel</div>
-          </nav>
-          <footer>
-            <h1 onClick={() => {
-              const {
-                name,
-                email
-              } = sign_up_form
+        <div className='ziltag-pseudo-comment'>
+          <ZiltagForm>
+            <input onChange={sign_up_form_name_changed} type='name' placeholder='Username'/>
+            <input onChange={sign_up_form_email_changed} type='email' placeholder='Email'/>
+            <nav>
+              <div onClick={pseudo_comment_sign_up} className='ziltag-ziltag-form__link ziltag-ziltag-form__link--activated'>Sign Up</div>
+              <div onClick={pseudo_comment_login} className='ziltag-ziltag-form__link'>Login</div>
+              <div onClick={deactivate_pseudo_comment} className='ziltag-ziltag-form__link' type='cancel'>Cancel</div>
+            </nav>
+            <footer>
+              <h1 onClick={() => {
+                const {
+                  name,
+                  email
+                } = sign_up_form
 
-              current_user_sign_up(name, email)
-              .then(() => {
-                fetch_current_user()
-                deactivate_pseudo_comment()
-              })
-            }}
-            className={'ziltag-ziltag-form__submit'}
-          >
-            Sign Up
-          </h1>
-            <p>Signing up means you agree with our <a>Terms</a> and <a>Privacy Policy</a>.</p>
-          </footer>
-        </ZiltagForm>
+                current_user_sign_up(name, email)
+                .then(() => {
+                  fetch_current_user()
+                  deactivate_pseudo_comment()
+                })
+              }}
+              className={'ziltag-ziltag-form__submit'}
+            >
+              Sign Up
+            </h1>
+              <p>Signing up means you agree with our <a>Terms</a> and <a>Privacy Policy</a>.</p>
+            </footer>
+          </ZiltagForm>
+        </div>
       )
     } else if (mode == 'login') {
       var component = (
-        <ZiltagForm>
-          <input onChange={login_form_user_changed} type='email' placeholder='Email'/>
-          <input onChange={login_form_password_changed} type='password' placeholder='Password'/>
-          <nav>
-            <div onClick={pseudo_comment_sign_up} className='ziltag-ziltag-form__link'>Sign Up</div>
-            <div onClick={pseudo_comment_login} className='ziltag-ziltag-form__link ziltag-ziltag-form__link--activated'>Login</div>
-            <div onClick={deactivate_pseudo_comment} className='ziltag-ziltag-form__link' type='cancel'>Cancel</div>
-          </nav>
-          <footer>
-            <h1 onClick={() => {
-              const {
-                user,
-                password
-              } = login_form
+        <div className='ziltag-pseudo-comment'>
+          <ZiltagForm>
+            <input onChange={login_form_user_changed} type='email' placeholder='Email'/>
+            <input onChange={login_form_password_changed} type='password' placeholder='Password'/>
+            <nav>
+              <div onClick={pseudo_comment_sign_up} className='ziltag-ziltag-form__link'>Sign Up</div>
+              <div onClick={pseudo_comment_login} className='ziltag-ziltag-form__link ziltag-ziltag-form__link--activated'>Login</div>
+              <div onClick={deactivate_pseudo_comment} className='ziltag-ziltag-form__link' type='cancel'>Cancel</div>
+            </nav>
+            <footer>
+              <h1 onClick={() => {
+                const {
+                  user,
+                  password
+                } = login_form
 
-              current_user_login(user, password)
-              .then(() => {
-                fetch_current_user()
-                deactivate_pseudo_comment()
-              })
-            }}
-            className={'ziltag-ziltag-form__submit'}
-          >
-            Login
-          </h1>
-            <p><a>Forget password?</a></p>
-          </footer>
-        </ZiltagForm>
+                current_user_login(user, password)
+                .then(() => {
+                  fetch_current_user()
+                  deactivate_pseudo_comment()
+                })
+              }}
+              className={'ziltag-ziltag-form__submit'}
+            >
+              Login
+            </h1>
+              <p><a>Forget password?</a></p>
+            </footer>
+          </ZiltagForm>
+        </div>
       )
     } else if (mode == 'read') {
       var component = (
@@ -118,6 +122,7 @@ class PseudoComment extends Component {
             </div>
             <div
               className='ziltag-ziltag-form__link'
+              type='cancel'
               onClick={deactivate_pseudo_comment}
             >
               Cancel
