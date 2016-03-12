25d8302dde82261de9dadf06a4c37c4ade19859d
floydwch <floydwch@gmail.com>
2016-03-12 09:10:39 +0800

    feat: support hotkey on Ziltag Comment Input

--- src/component/ZiltagCommentInput/index.js
+++ src/component/ZiltagCommentInput/index.js
@@ -22,6 +22,24 @@ class ZiltagCommentInput extends Component {
       activate_pseudo_comment
     } = this.props
 
+    const {
+      ctrl_pressed
+    } = this.state || {}
+
+    function is_ctrl(keyCode) {
+      return keyCode == 91 || keyCode == 17
+    }
+
+    function post() {
+      if (usr) {
+        if (ziltag_comment_input.content) {
+          onSubmit()
+        }
+      } else {
+        activate_pseudo_comment()
+      }
+    }
+
     return (
       <div className='ziltag-ziltag-comment-input'>
         <Avatar
@@ -30,19 +48,25 @@ class ZiltagCommentInput extends Component {
         />
         <TextareaAutosize
           onChange={onChange}
+          onKeyDown={(e) => {
+            if (is_ctrl(e.keyCode)) {
+              this.setState({ctrl_pressed: true})
+            }
+            if (ctrl_pressed && e.key == 'Enter') {
+              post()
+              e.preventDefault()
+            }
+          }}
+          onKeyUp={(e) => {
+            if (is_ctrl(e.keyCode)) {
+              this.setState({ctrl_pressed: false})
+            }
+          }}
           value={ziltag_comment_input.content || ''}
           placeholder='Your text here'
         />
         <div
-          onClick={() => {
-            if (usr) {
-              if (ziltag_comment_input.content) {
-                onSubmit()
-              }
-            } else {
-              activate_pseudo_comment()
-            }
-          }}
+          onClick={post}
           className={classNames({
             'ziltag-ziltag-comment-input__post': true,
             'ziltag-ziltag-comment-input__post--activated':
