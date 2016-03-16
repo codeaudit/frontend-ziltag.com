183a0401110c01a33d4d3b49141f41a1f4e5822f
floydwch <floydwch@gmail.com>
2016-03-16 14:26:28 +0800

    fix: fix only one child case in ZiltagMapDialog

--- src/component/ZiltagMapDialog/index.js
+++ src/component/ZiltagMapDialog/index.js
@@ -14,33 +14,43 @@ class ZiltagMapDialog extends Component {
       children
     } = this.props
 
-    const group = children.reduce((prev, curr) => {
-      if (curr.type) {
-        if (typeof curr.type == 'string') {
-          var type = curr.type
-        } else if (typeof curr.type == 'function') {
-          var type = curr.type.name
+    if (Array.isArray(children)) {
+      var group = children.reduce((prev, curr) => {
+        if (curr.type) {
+          if (typeof curr.type == 'string') {
+            var type = curr.type
+          } else if (typeof curr.type == 'function') {
+            var type = curr.type.name
+          }
         }
-      }
 
-      if (!prev[type]) {
-        prev[type] = []
-      }
-      prev[type].push(curr)
+        if (!prev[type]) {
+          prev[type] = []
+        }
+        prev[type].push(curr)
 
-      return prev
-    }, {})
+        return prev
+      }, {})
+    }
 
-    return (
-      <div className='ziltag-ziltag-map-dialog'>
-        <div className='ziltag-ziltag-map-dialog__col0'>
-          {group['p']}
+    if (group) {
+      return (
+        <div className='ziltag-ziltag-map-dialog'>
+          <div className='ziltag-ziltag-map-dialog__col0'>
+            {group['p']}
+          </div>
+          <div className='ziltag-ziltag-map-dialog__col1'>
+            {group['nav']}
+          </div>
         </div>
-        <div className='ziltag-ziltag-map-dialog__col1'>
-          {group['nav']}
+      )
+    } else {
+      return (
+        <div className='ziltag-ziltag-map-dialog'>
+          {children}
         </div>
-      </div>
-    )
+      )
+    }
   }
 }
 
