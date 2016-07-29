586c050fbb0bb9b8e5c64976e7a2e086ac930e90
floydwch <floydwch@gmail.com>
2016-07-29 08:31:45 +0800

    fix: fix routes

--- src/route.js
+++ src/route.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Route from 'react-router'
+import {Route} from 'react-router'
 
 import ZiltagMapPage from './page/ZiltagMapPage'
 import ZiltagPage from './page/ZiltagPage'
