d1d5103eb02dbf09e47bc64a8ddfdd132527e6af
floydwch <floydwch@gmail.com>
2016-09-03 10:51:08 +0800

    chore: eliminate cssnano since it's already included in css-loader
    close #230

--- webpack/base.config.babel.js
+++ webpack/base.config.babel.js
@@ -1,7 +1,6 @@
 import path from 'path'
 
 import webpack from 'webpack'
-import cssnano from 'cssnano'
 import postcss_cssnext from 'postcss-cssnext'
 
 import {NODE_ENV} from '../env'
@@ -37,6 +36,6 @@ module.exports = {
     })
   ],
   postcss: () => {
-    return [postcss_cssnext, cssnano]
+    return [postcss_cssnext]
   }
 }
